271ea77563a66805adca6af9e00c8db2fcf2a0a4
Eugene Marcotte <eugene.marcotte@takeoff.com>
2021-08-04 17:14:01 -0400

    more debug

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -8,7 +8,7 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = github.getOctokit(token);
-    console.log(octokit);
+    console.log(octokit.issues);
 
     const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
